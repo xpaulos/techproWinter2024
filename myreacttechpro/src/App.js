e323913f55c500b249a5766cdfbc9748b7eab842
Dimitris <tasioudisdimitrios@gmail.com>
2024-10-14 20:07:38 +0300

    changes

--- myreacttechpro/src/App.js
+++ myreacttechpro/src/App.js
@@ -9,7 +9,7 @@ function App() {
         
         {/* Type your changes below this line */}
 
-        <p>  Pavlos Xouplidis       </p>
+        <p>  Dimitrios Tasioudis      </p>
 
         {/* Type your changes above this line*/}
 
