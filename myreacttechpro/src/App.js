05218fc67f62b319a0368ead0175be874c5ba90c
Theodoros Lioupis <tklioupis@gmail.com>
2024-10-14 20:26:01 +0300

    made changes 3 - Theodoros Lioupis

--- myreacttechpro/src/App.js
+++ myreacttechpro/src/App.js
@@ -9,12 +9,8 @@ function App() {
 
         {/* Type your changes below this line */}
 
-<<<<<<< HEAD
-        <p>  Pavlos Xouplidis       </p>
-        <p>  Marios Goutidis</p>
-=======
->>>>>>> 48b9805cf742d4eed7193e16427e81d9a6b8c98e
-
+        <p>  Theodoros Lioupis       </p>
+      
         {/* Type your changes above this line*/}
 
         <a
