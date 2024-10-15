995b22b0ed94d0909c3dd45aa051ebadf4ea50f1
konstantina <konstantinadamianou@gmail.com>
2024-10-15 19:08:56 +0300

    my name

--- myreacttechpro/src/App.js
+++ myreacttechpro/src/App.js
@@ -9,7 +9,7 @@ function App() {
         
         {/* Type your changes below this line */}
 
-        <p>  Pavlos Xouplidis       </p>
+        <p>  konstantina damianou       </p>
 
         {/* Type your changes above this line*/}
 
